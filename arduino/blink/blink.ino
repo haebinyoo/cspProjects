3df5eca4fb641086c5d8958aee68b64f908c6d74
Haebin Yoo <h.yoo@students.chapelfield.org>
2019-02-14 14:55:58 -0500

    Add comments to code

--- arduino/blink/blink.ino
+++ arduino/blink/blink.ino
@@ -1,12 +1,24 @@
 void setup() {
   // put your setup code here, to run once:
-  pinMode(13, OUTPUT);
+  pinMode(13, OUTPUT); //setting pin 13 to output
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(13, HIGH);
-  delay(1000);
-  digitalWrite(13, LOW);
-  delay(2000);
+  digitalWrite(13, HIGH); //turn on the light
+  delay(1000); //wait 1 second
+  digitalWrite(13, LOW); //turn off the light
+  delay(2000); //wait 2 seconds
+  digitalWrite(13, HIGH); //turn on the light 
+  delay(3000); //wait 3 seconds
+  digitalWrite(13, LOW); //turn off the light
+  delay(4000); //wait 4 seconds
+  digitalWrite(13, HIGH); //turn on the light
+  delay(5000); // wait 5 seconds
+  digitalWrite(13, LOW); //turn off the light
+  delay(5000); // wait 5 seconds
+  digitalWrite(13, HIGH); //turn on the light
+  delay(4000); // wait 4 seconds
+  digitalWrite(13, LOW); //turn off the light
+  delay(3000); // wait for 3 seconds
 }
