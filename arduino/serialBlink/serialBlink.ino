d4dad2eea530f8b4651be2ea7afbb00e867a57f3
Haebin Yoo <h.yoo@students.chapelfield.org>
2019-03-28 14:51:20 -0400

    Add all the comments

--- arduino/serialBlink/serialBlink.ino
+++ arduino/serialBlink/serialBlink.ino
@@ -1,18 +1,28 @@
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
+  Serial.begin(9600);//begin the serial
+  Serial.setTimeout(10);
   pinMode(13, OUTPUT);
-  Serial.println("Good Morning"); 
+  Serial.println("Good Morning");
   Serial.println("Send 'On' to turn on the LED.");
   Serial.println("Send 'Off' to turn off the LED.");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(13, HIGH);
-  Serial.println("On");
-  delay(1000);
-  digitalWrite(13, LOW);
-  Serial.println("Off");
-  delay(1000);
-}
+  if (Serial.available() > 0) {
+    String command;
+    command = Serial.readString();
+    command.trim();
+    if (command == "On") {
+      Serial.println("When type On, the light is on");
+        digitalWrite(13, HIGH);
+        Serial.println("The light is on");
+    }
+      else if (command == "Off") {
+        Serial.println("When type Off, the light is off"); 
+          digitalWrite(13, LOW);
+          Serial.println("The light is off");
+      }
+
+  }  }
