8c41455e3cb18915c87db2303a5d2b69a35729c2
Haebin Yoo <h.yoo@students.chapelfield.org>
2019-04-09 14:00:52 -0400

    Add comments

--- arduino/ifFade/ifFade.ino
+++ arduino/ifFade/ifFade.ino
@@ -1,17 +1,17 @@
-int lightValue = 0;
-int delta = 1;
+int lightValue = 0; // the integer of the lightValue is 0
+int delta = 1; // the integer of the delta is 1
 
 void setup() {
-  Serial.begin(9600);
-  pinMode(9, OUTPUT);
+  Serial.begin(9600); // begin the serial
+  pinMode(9, OUTPUT); // the mode will be #9
 }
 
 void loop() {
-  lightValue = lightValue + delta;
-  if (lightValue <= 0 || lightValue >= 255) {
-    delta = -delta;
-  }
-  analogWrite(9, lightValue);
-  Serial.println(lightValue);
-  delay(10);
+  lightValue = lightValue + delta; // lightValue add delta will equal the new lightValue
+  if (lightValue <= 0 || lightValue >= 255) // when the lightValue is less than 0 or more than 255 {
+    delta = -delta; // the delta will equal negative
+}
+analogWrite(9, lightValue); // Output lightValue to the LED
+Serial.println(lightValue); // Output lightValue to the serial monitor
+delay(10); // wait 1 millisecond
 }
