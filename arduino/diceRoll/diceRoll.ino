6561efadab941088e22533ba75409e3259ce1351
Haebin Yoo <h.yoo@students.chapelfield.org>
2019-04-25 14:29:49 -0400

    Declare global variables

--- arduino/diceRoll/diceRoll.ino
+++ arduino/diceRoll/diceRoll.ino
@@ -1,10 +1,24 @@
 void setup() {
   Serial.begin(115200);
+  randomSeed(analogRead(0));
 }
 
 void loop() {
+  long randNumber;
+  boolean stop_if;
+  stop_if = false;
   randNumber = random(1, 7);
   Serial.println(randNumber);
 
   delay(1000);
+
+  Serial.println("Roll Again?");
+  long command;
+  if (command == "Yes") {
+    Serial.println(randNumber);
+  }
+  else if (command == "No") {
+    stop_if = true;
+    return stop_if;
+  }
 }
--- arduino/ifFade/ifFade.ino
+++ arduino/ifFade/ifFade.ino
@@ -1,5 +1,9 @@
-//the integer of the lightValue is 0 and the integer of the delta is 1 begin the serial and the mode will be #9 when the lightValue is less than 0 or more than 255 the
-//delta is going to be negative so when is reaches its limit, the number is going to go down or up and it will keep going in a loop and send the output to both the serial
+//the integer of the lightValue is 0 and the integer of the 
+//delta is 1 begin the serial and the mode will be #9 when 
+//the lightValue is less than 0 or more than 255 the
+//delta is going to be negative so when is reaches its limit,
+//the number is going to go down or up and it will keep 
+//going in a loop and send the output to both the serial
 //monitor and to the LED and wait for 10 milliseconds.
 
 int lightValue = 0;
