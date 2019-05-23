e7117ef1aaebe4710143048a1598f58e655d1526
Haebin Yoo <h.yoo@students.chapelfield.org>
2019-05-23 15:01:39 -0400

    Finalize the work

--- arduino/serialCalculate/serialCalculate.ino
+++ arduino/serialCalculate/serialCalculate.ino
@@ -25,18 +25,15 @@ void cleanData() {
     if (isDigit(Serial.peek()) == true) {
       integers[count] = Serial.parseInt();
       count++;
-      Serial.print("The current count is ");
-      Serial.println(count);
       delay(1);
-
     }
     else if (isGraph(Serial.peek()) == true) {
       sign = Serial.read();
-      Serial.println(sign);
       delay(1);
     }
     else {
       Serial.read();
+      delay(1);
     }
   }
 }
@@ -44,36 +41,28 @@ void parseData() {
   if (newData == true) {
     number1 = integers[0];
     number2 = integers[1];
-    Serial.println(number1);
-    Serial.println(sign);
-    Serial.println(number2);
     newData = false;
-    readyToCalculate = true; 
+    readyToCalculate = true;
   }
 }
 void calculate() {
   if (readyToCalculate == true) {
-    Serial.println("Calculating...");
     switch (sign) {
       case '+':
         result = number1 + number2;
         readyToPrint = true;
-        Serial.print("Addition");
         break;
       case '-':
         result = number1 - number2;
         readyToPrint = true;
-        Serial.print("Substraction");
         break;
       case '*':
         result = number1 * number2;
         readyToPrint = true;
-        Serial.print("Multiplication");
         break;
       case '/':
         result = number1 / number2;
         readyToPrint = true;
-        Serial.print("Division");
         break;
       default:
         Serial.println("This operation is not supported. Please try again.");
@@ -90,5 +79,8 @@ void concatenateResultsAndPrint() {
     String printCalculation = String(number1) + ' ' + String(sign) + ' ' + String(number2) + String(" = ") + String(result);
     Serial.println(printCalculation);
     readyToPrint = false;
+    number1 = ' ';
+    number2 = ' ';
+    sign = ' ';
   }
 }
